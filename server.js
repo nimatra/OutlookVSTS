e6dd72f07f09e3d9dee35e45e308244d838ba256
nimatra <nimatra@gmail.com>
2016-05-21 15:35:30 -0700

    node updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var webpack = require('webpack');
+var bodyParser = require('body-parser');
 var config = require('./config/webpack.prod');
 var authenticate = require('./routes/authenticate');
 var vsts = require('./routes/vsts');
@@ -8,6 +9,11 @@ var vsts = require('./routes/vsts');
 var app = express();
 var compiler = webpack(config);
 var port = process.env.PORT || 3000;
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
