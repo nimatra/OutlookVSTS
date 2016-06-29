82057352445f32567009d6c9708c8b90cc450aa2
Miranda McClellan <t-mimccl@microsoft.com>
2016-06-29 13:58:42 -0700

    LogInPage correctly calls auth. Settings has basic buttons/headings/dropdowns

---
+++ App/VSTS/LogInPage.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { Provider } from 'react-redux';
+// import { Office } from 'Office';
+import {Authenticate } from '../Authenticate/authenticate';
+import { Store, createStore } from 'redux';
+
+export class LogInPage extends React.Component<{}, {isReady : boolean}> {
+
+  //Work around for Office slow to initialize w/ error:
+  //Office.js:12 Uncaught Office.js has not been fully loaded yet. Please try again later or make sure to add your initialization code on the Office.initialize function.
+  isReady : boolean; // set to false
+
+  private Initialize():void{
+    this.isReady = true;
+    this.forceUpdate(); //re-renders page
+  }
+
+  public constructor() {
+    super(); //required first line
+    this.isReady = true;
+    Office.initialize = this.Initialize;
+  }
+
+  private auth(): void{
+    var user = Office.context.mailbox.userProfile.emailAddress;
+    window.open('./authenticate?user=' + user);
+    //return (<Authenticate user = {user}/>);
+  }
+
+  public render(): React.ReactElement<Provider> {
+    if(this.isReady == false)
+    {
+      return(<div>loading...</div>);
+    }
+
+    //add CSS in folder for reuse in other properties
+    var style_img = {
+      align: 'center'
+    };
+
+    var style_button = {
+      backgroundcolor: 'rgb(0,122,204)', // save button blue
+      textalign: 'center',
+      textcolor: 'rgb(255,255,255)',
+      font: "20px arial, sans-serif",
+      align: 'center'
+    };
+
+    var style_section = {
+      color: 'rgb(104,33,122)', // the VS purple
+      font: "20px arial, sans-serif",
+
+    };
+
+    var style_text = {
+       color: "rgb(30,30,30)", // TODO - change to dark gray
+       font: "15px arial, sans-serif",
+    };
+
+    var style_bottomlogo = {
+      width:'500px',
+      height:'120px',
+      align: 'center'
+    };
+
+
+    console.log('got to login');
+    return (
+      <div>
+      <div> logo
+      </div>
+      <div><button onClick={this.auth} style = {style_button}>Sign In</button></div>
+      <div> line separator</div>
+      <div>
+        <h1 style = {style_section}> Create work items</h1>
+        <p  style = {style_text}> Do you have an email thread you need to make into a work item? Create work items directly from Outlook!</p>
+      </div>
+      <div>
+        <h1 style = {style_section}> Communicate with your team</h1>
+        <p style = {style_text}> After creating a work item, you can reply-all the thread with the item information or copy the information to the clipboard.</p>
+      </div>
+      <div>bottom image</div>
+      </div>
+    );
+  }
+ }
--- App/VSTS/Settings.tsx
+++ App/VSTS/Settings.tsx
@@ -28,7 +28,6 @@ export class Settings extends React.Component<{}, {}> {
       font: "20px arial, sans-serif",
       align: 'center'
     };
-
     console.log('got to settings');
     return (
       <div>
