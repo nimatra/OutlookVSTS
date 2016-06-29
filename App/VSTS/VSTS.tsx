b6423988544593b1c67a13b7fc3376bb3c73caea
Miranda McClellan <t-mimccl@microsoft.com>
2016-06-28 17:07:02 -0700

    updated VSTS.tsx to render log-in page

--- App/VSTS/VSTS.tsx
+++ App/VSTS/VSTS.tsx
@@ -3,17 +3,29 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import {LogInPage } from './LogInPage';
 import {Settings} from './Settings';
+import {AuthState} from './auth';
 
 enum Users { None, EmilyT, EmilyZ, Miranda}
 
-export class VSTS extends React.Component<{}, {user: Users}> {
+export class VSTS extends React.Component<{}, any/*{user: Users}*/> {
 
   public constructor(){
     super();
-    this.state = {user:Users.None};
+    this.state = {user:Users.None,
+      ready:false
+    };
     this.setEmilyT = this.setEmilyT.bind(this);
     this.setEmilyZ = this.setEmilyZ.bind(this);
     this.setMiranda = this.setMiranda.bind(this);
+    this.Initialize = this.Initialize.bind(this);
+    Office.initialize = this.Initialize;
+  }
+
+  private Initialize():void{
+    console.log("Initiating");
+    this.setState({ready:true});
+    console.log(this.state.ready);
+    //this.forceUpdate();
   }
 
   public setEmilyT():void{this.setState({user:Users.EmilyT})}
@@ -21,6 +33,8 @@ export class VSTS extends React.Component<{}, {user: Users}> {
   public setMiranda():void{this.setState({user:Users.Miranda})}
 
   public render(): React.ReactElement<Provider> {
+    if(!this.state.ready)
+      return(<div>Loading</div>);
 
     console.log('got to vsts');
     const user : Users = this.state.user;
