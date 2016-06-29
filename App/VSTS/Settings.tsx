ec455a47256135bd4d1974b67123def2ec93d9be
Miranda McClellan <t-mimccl@microsoft.com>
2016-06-29 14:48:05 -0700

    Update Settings to have dropdowns

--- App/VSTS/Settings.tsx
+++ App/VSTS/Settings.tsx
@@ -1,11 +1,31 @@
 /// <reference path="../../office.d.ts" />
 import * as React from 'react';
 import { Provider } from 'react-redux';
-//setting page
+
 export class Settings extends React.Component<{}, {}> {
+
 
   public render(): React.ReactElement<Provider> {
     var style_img = {
+      align: 'center'
+    };
+
+    var style_section = {
+      color: 'rgb(104,33,122)', // the VS purple
+      font: "20px arial, sans-serif",
+
+    };
+
+    var style_text = {
+       color: "rgb(30,30,30)", // TODO - change to dark gray
+       font: "15px arial, sans-serif",
+    };
+
+    var style_button = {
+      backgroundcolor: 'rgb(0,122,204)', // save button blue
+      textalign: 'center',
+      color: 'rgb(255,255,255)',
+      font: "20px arial, sans-serif",
       align: 'center'
     };
 
@@ -15,21 +35,21 @@ export class Settings extends React.Component<{}, {}> {
           <div> logo</div>
           <div>
             <h1> Default Settings </h1>
-            <p> Assign default values for work item creation</p>
+            <p style = {style_text}> Assign default values for work item creation</p>
             line separator
           </div>
           <div>
-            <button> Save </button>
+            <button style={style_button}> Save </button>
           </div>
           <div>
-            <h3 style= {style_h3}>Account</h3>
-            <select name ="">
+            <h3 style= {style_section} size = '6'>Account</h3>
+            <select name ="account">
             </select>
-            <h3 style= {style_h3}>Project</h3>
-            <select name ="">
+            <h3 style= {style_section} size = '6'>Project</h3>
+            <select name ="project">
             </select>
-            <h3 style= {style_h3}>Area</h3>
-            <select name ="">
+            <h3 style= {style_section} size = '6'>Area</h3>
+            <select name ="area">
             </select>
           </div>
       </div>
