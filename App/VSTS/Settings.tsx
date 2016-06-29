82659977dc55810fefe87aa18cf15b859d97378a
Miranda McClellan <t-mimccl@microsoft.com>
2016-06-29 13:58:42 -0700

    LogInPage correctly calls auth. Settings has basic buttons/headings/dropdowns

--- App/VSTS/Settings.tsx
+++ App/VSTS/Settings.tsx
@@ -9,12 +9,30 @@ export class Settings extends React.Component<{}, {}> {
       align: 'center'
     };
 
-
-    console.log('got to vsts');
+    console.log('got to settings');
     return (
       <div>
-          <p> Settings Page to Come</p>
+          <div> logo</div>
+          <div>
+            <h1> Default Settings </h1>
+            <p> Assign default values for work item creation</p>
+            line separator
           </div>
-          );
+          <div>
+            <button> Save </button>
+          </div>
+          <div>
+            <h3 style= {style_h3}>Account</h3>
+            <select name ="">
+            </select>
+            <h3 style= {style_h3}>Project</h3>
+            <select name ="">
+            </select>
+            <h3 style= {style_h3}>Area</h3>
+            <select name ="">
+            </select>
+          </div>
+      </div>
+      );
   }
  }
