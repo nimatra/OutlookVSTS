56b92cdf406e8c6929c5a36638da77c396ec53db
Miranda McClellan <t-mimccl@microsoft.com>
2016-06-30 09:52:30 -0700

    Added images to UI Mockups

--- App/VSTS/LogInPage.tsx
+++ App/VSTS/LogInPage.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-// import { Office } from 'Office';
-import {Authenticate } from '../Authenticate/authenticate';
 import { Store, createStore } from 'redux';
 import {Settings } from './Settings';
 
-export class LogInPage extends React.Component<{}, {isReady : boolean}> {
+export class LogInPage extends React.Component<{}, {}> {
 
   isReady : boolean; // set to false
 
@@ -23,7 +21,6 @@ export class LogInPage extends React.Component<{}, {isReady : boolean}> {
   private auth(): void{
     var user = Office.context.mailbox.userProfile.emailAddress;
     window.open('./authenticate?user=' + user);
-    //return (<Authenticate user = {user}/>);
   }
 
   public render(): React.ReactElement<Provider> {
@@ -34,41 +31,40 @@ export class LogInPage extends React.Component<{}, {isReady : boolean}> {
 
     //add CSS in folder for reuse in other properties
     var style_img = {
-      align: 'center'
+      align: 'center',
     };
 
     var style_button = {
-      backgroundcolor: 'rgb(0,122,204)', // save button blue
+      background: 'rgb(0,122,204)', // save button blue
       textalign: 'center',
       color: 'rgb(255,255,255)',
       font: "20px arial, sans-serif",
-      align: 'center'
+      align: 'center',
     };
 
     var style_section = {
       color: 'rgb(104,33,122)', // the VS purple
       font: "20px arial, sans-serif",
-
     };
 
     var style_text = {
-       color: "rgb(30,30,30)", // TODO - change to dark gray
+       color: "rgb(118,118,118)", // TODO - change to dark gray
        font: "15px arial, sans-serif",
     };
 
     var style_bottomlogo = {
       width:'500px',
       height:'120px',
-      align: 'center'
+      align: 'center',
     };
 
 
     console.log('got to login');
     return (
       <div>
-      <div> logo</div>
+      <div><image src = './images/logo.png' style = {style_img}/></div>
       <div><button onClick={this.auth} style = {style_button}>Sign In</button></div>
-      <div> line separator</div>
+      <hr/>
       <div>
         <h1 style = {style_section}> Create work items</h1>
         <p  style = {style_text}> Do you have an email thread you need to make into a work item? Create work items directly from Outlook!</p>
@@ -77,7 +73,7 @@ export class LogInPage extends React.Component<{}, {isReady : boolean}> {
         <h1 style = {style_section}> Communicate with your team</h1>
         <p style = {style_text}> After creating a work item, you can reply-all the thread with the item information or copy the information to the clipboard.</p>
       </div>
-      <div>bottom image</div>
+      <div><image src = './images/logo_strip.png' style = {style_bottomlogo}/> </div>
       <Settings />
       </div>
     );
