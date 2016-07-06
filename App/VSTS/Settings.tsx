ba3702e7954e83bec5fa0078b2e6d8d1996a0183
Miranda McClellan <t-mimccl@microsoft.com>
2016-07-05 17:19:27 -0700

    Moved wait for Office initialize to VSTS main page

--- App/VSTS/Settings.tsx
+++ App/VSTS/Settings.tsx
@@ -1,8 +1,22 @@
 /// <reference path="../../office.d.ts" />
 import * as React from 'react';
 import { Provider } from 'react-redux';
+import {LogInPage } from './LogInPage';
 
-export class Settings extends React.Component<{}, {}> {
+export class Settings extends React.Component<{}, {saved:boolean}> {
+
+  public constructor() {
+    super();
+    this.state = {saved:false};
+    console.log(this.state);
+  }
+
+  private save(): void{
+    console.log('saving');
+    this.setState({saved:true});
+    this.forceUpdate();
+    //TODO - save defaults to roaming settings
+  }
 
   public render(): React.ReactElement<Provider> {
     var style_img = {
@@ -12,11 +26,10 @@ export class Settings extends React.Component<{}, {}> {
     var style_section = {
       color: 'rgb(104,33,122)', // the VS purple
       font: "20px arial, sans-serif",
-
     };
 
     var style_text = {
-       color: "rgb(118,118,118)", // TODO - change to dark gray
+       color: "rgb(118,118,118)", // dark gray
        font: "15px arial, sans-serif",
     };
 
@@ -28,7 +41,9 @@ export class Settings extends React.Component<{}, {}> {
       align: 'center',
     };
     console.log('got to settings');
-    return (
+    console.log(this.state);
+    if(this.state.saved == false){
+      return (
       <div>
           <div> <image src = './images/logo.png' style = {style_img}/></div>
           <div>
@@ -36,21 +51,28 @@ export class Settings extends React.Component<{}, {}> {
             <p style = {style_text}> Assign default values for work item creation</p>
           </div>
           <div>
-            <button style={style_button}> Save </button>
+            <button style={style_button} onclick = {window.close}> Save </button>
           </div>
           <hr/>
           <div>
             <h3 style= {style_section} size = '6'>Account</h3>
             <select name ="account">
+              <option value="o365">o365exchange.visualstudio.com</option>
             </select>
             <h3 style= {style_section} size = '6'>Project</h3>
             <select name ="project">
+              <option value="outlook">Outlook Services</option>
             </select>
             <h3 style= {style_section} size = '6'>Area</h3>
             <select name ="area">
+              <option value="vsts">VSTS</option>
             </select>
           </div>
       </div>
-      );
+      );}
+     else
+     {
+       return (<div>Create Item </div>);
+     }
   }
  }
