52f907592e3520e3df34ef448d78e47a24b45d63
Emily Zhou <t-emzhou@microsoft.com>
2016-08-02 14:49:06 -0700

    made updates per Ayushis code review

--- App/VSTS/CopyButton.tsx
+++ App/VSTS/CopyButton.tsx
@@ -1,33 +1,55 @@
 /// <reference path="../../office.d.ts" />
 import * as React from 'react';
 
-
+/**
+ * Props for CopyButton Component
+ * @interface { ICopyButtonProps }
+ */
 interface ICopyButtonProps {
+  /**
+   * workItemHyperlink
+   * @type { string }
+   */
   workItemHyperlink: string;
 }
 
+/**
+ * Renders a button that writes an HTML element to clipboard on-click
+ * @class { CopyButton }
+ */
 export class CopyButton extends React.Component<ICopyButtonProps, {}> {
-
+  /**
+   * Renders the CopyButton Component and reads ICopyButtonProps
+   * Returns ReactHTML 
+   */
   public render(): React.ReactElement<{}> {
     console.log('got to copybutton');
-    document.addEventListener('copy', this.setClipboardHandler);
-
+    document.addEventListener('copy', this.setClipboardData);
     return (
       <div>
-      <button className='ms-Button' onClick={this.handleClick}>
-      <span className='ms-Icon ms-Icon--copy'></span>
-      {'   '}Copy to Clipboard
-      </button>
-      <h1></h1>
+        <button className='ms-Button' onClick={this.handleClick}>
+          <a className='ms-Icon ms-Icon--copy'/>
+          {'   '}Copy to Clipboard
+        </button>
+        <br/><br/>
       </div>
     );
   }
 
-  private handleClick: any = (event) => {
+  /**
+   * Handles the button click and fires a copy command
+   * @private
+   */
+  private handleClick: () => void = () => {
     document.execCommand('copy');
   }
 
-  private setClipboardHandler: any = (e: any) => {
+  /**
+   * Writes the formatted HTML element to the clipboard
+   * @private
+   * @oaran { any } e 
+   */
+  private setClipboardData: (e: any) => void = (e) => {
     console.log('got to handler');
     e.clipboardData.setData('text/html', this.props.workItemHyperlink);
     e.preventDefault(); // we want our data, not data from any selection, to be written to the clipboard
